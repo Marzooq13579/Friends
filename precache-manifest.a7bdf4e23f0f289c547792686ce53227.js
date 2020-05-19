e8c3ff8b8da44b642c2a0d3b1453f69b5ee91949
Marzooq13579 <marzooqabs@gmail.com>
2020-05-19 17:52:23 +0530

    Updates

---
+++ precache-manifest.a7bdf4e23f0f289c547792686ce53227.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6186871eaea2931675f750e5052e3ef",
+    "url": "/Friends/index.html"
+  },
+  {
+    "revision": "5835a42b993cb91b682c",
+    "url": "/Friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d0c8b6f86c5b16012a7e",
+    "url": "/Friends/static/css/main.adc93536.chunk.css"
+  },
+  {
+    "revision": "5835a42b993cb91b682c",
+    "url": "/Friends/static/js/2.8eb3436a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Friends/static/js/2.8eb3436a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c8b6f86c5b16012a7e",
+    "url": "/Friends/static/js/main.09ca594c.chunk.js"
+  },
+  {
+    "revision": "8b4b0961d1d96971414f",
+    "url": "/Friends/static/js/runtime-main.106da50a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
